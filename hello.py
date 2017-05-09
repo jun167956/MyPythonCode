95fded710c18190e172a6ebe3b705c5e1c32a261
jun167956 <17785607530@163.com>
2017-05-09 23:56:38 +0800

    今天进展较慢，只完成了循环练习

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-print("Hello World!")
+'''print("Hello World!")
 a,b,c,d = 10,5.5,True,1+2j
-print(type(a), type(b), type(c), type(d))
+print(type(a), type(b), type(c), type(d))'''
+
+numbers = [12,13,14,15,16,17]
+odd = []
+even = []
+while len(numbers) > 0:
+    number = numbers.pop()
+    if number % 2 == 0:
+        odd.append(number)
+    else:
+        even.append(number)
+print(odd)
+print(even)
+
